747a75d087f5b92a6a2c3106b3eaaf62262aba53
hugoabonizio <hugo_abonizio@hotmail.com>
2014-10-03 15:28:27 +0000

    add specs

--- lib/ello.rb
+++ lib/ello.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'logger'
 
 class Ello
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :friends_page
   
   LOGIN_URL = 'https://ello.co/enter'
   
@@ -15,18 +15,21 @@ class Ello
   def authenticate!
     login_page = HTTP.get(LOGIN_URL)
     html = Nokogiri::HTML(login_page.to_s)
-    csrf_token = html.xpath("//meta[@name='csrf-token']/@content").first
+    @csrf_token = html.xpath("//meta[@name='csrf-token']/@content").first
     
     form = {
       'utf8' => '✓',
-      'authenticity_token' => csrf_token.to_s,
+      'authenticity_token' => @csrf_token.to_s,
       'user[email]' => @email,
       'user[password]' => @password,
       'user[remember_me]' => 0,
       'user[remember_me]' => 1,
       'commit' => 'Enter Ello' 
     }
-    HTTP.with_headers('Cookie' => login_page.headers['Set-Cookie']).post(LOGIN_URL, form: form)
-    #form
+    @friends_page = HTTP.with_headers('Cookie' => login_page.headers['Set-Cookie']).post(LOGIN_URL, form: form)
+  end
+  
+  def publications
+    
   end
 end
--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -6,4 +6,10 @@ describe "Authenticate methods" do
     expect(ello.email).to eql('email@domain.com')
     expect(ello.password).to eql('password')
   end
+  
+  it "should redirect after login" do
+    ello = Ello.new(ENV['ELLO_USER'], ENV['ELLO_PASSWORD'])
+    ello.authenticate!
+    expect(ello.authenticate!.to_s).to include('<html><body>You are being')
+  end
 end
